62504876f570279713eded6261644ac93ffa07af
Alain Natalini <the_klingon@live.it>
2018-10-05 11:08:32 +0200

    first commit

---
+++ StartDataPersistence/StartDataPersistence/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace StartWithDataPersistence
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (Application.Current.Properties.ContainsKey("Name"))
+                userNickName.Text = Application.Current.Properties["Name"].ToString();
+
+            if (Application.Current.Properties.ContainsKey("NotificationsEnabled"))
+                notificationEnabled.On = (bool)Application.Current.Properties["NotificationsEnabled"];
+
+            if (Application.Current.Properties.ContainsKey("AirplaneModeEnabled"))
+                airplaneModeEnabled.On = (bool)Application.Current.Properties["AirplaneModeEnabled"];
+
+        }
+
+        private void OnChange(object sender, EventArgs e)
+        {
+            Application.Current.Properties["Name"] = userNickName.Text;
+            Application.Current.Properties["NotificationsEnabled"] = notificationEnabled.On;
+            Application.Current.Properties["AirplaneModeEnabled"] = airplaneModeEnabled.On;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+    }
+}
